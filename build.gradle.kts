7edceaa148b8b4ab8b39373657ee92d9d3bf6c58
Richard Harrah <topplethenunnery@gmail.com>
2021-02-05 19:15:53 -0500

    update to pixeloutlaw-gradle-plugin 2.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm")
     id("nebula.release") version "15.3.0"
     id("nebula.nebula-bintray") version "8.5.0"
-    id("io.pixeloutlaw.multi") version "0.5.0"
-    id("io.pixeloutlaw.spigot.build") version "0.5.0"
+    id("io.pixeloutlaw.multi") version "2.0.5"
+    id("io.pixeloutlaw.spigot.build") version "2.0.5"
 }
 
 bintray {
@@ -15,6 +15,7 @@ bintray {
 
 spigotBuildTools {
     versions = listOf(
+        "1.16.5",
         "1.16.4",
         "1.16.3",
         "1.16.2",
